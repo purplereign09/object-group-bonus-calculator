26b89d558eada3dd726bbaf62bba14ab91d99b9a
Patty Baraibar <pattybaraibar@msn.com>
2021-07-20 16:26:57 -0500

    finalized project

--- client.js
+++ client.js
@@ -48,48 +48,71 @@ for(let employee of employees){
   console.log(employee);
 }
 
-// function newCar(yearInput, makeInput, modelInput){
-//   console.log('in newCar:', yearInput, makeInput, modelInput);
-//   const newCarObject = {
-//     year: yearInput,
-//     make: makeInput,
-//     model: modelInput
-//   }
-//   garage.push(newCarObject);
-//   return true;
-// } // end newCar
 
 
 //function that takes in one employee object
 function bonusCalculator(name){
   const individualBonus = [];
-    //property should contain the bonus percentage the employee is to receive. See section below for calculation
-  for(let employee of employees){
-    if(name === employee.name){
-      individualBonus.push(employee.name);
-        let bonusPercent = 0;
-        if (employee.reviewRating <= 2) {
-          bonusPercent = 0;
-        } else if (employee.reviewRating === 3) {
-          bonusPercent = (4 / 100) * Number(employee.annualSalary);
-        } else if (employee.reviewRating === 4) {
-          bonusPercent = (6 / 100) * Number(employee.annualSalary);
-        } else if (employee.reviewRating === 5) {
-         bonusPercent = (10 / 100) * Number(employee.annualSalary);
-        }
-        individualBonus.push(bonusPercent); 
+  //empty array to hold the individual bonus values
+  for (let employee of employees) {
+    //loops through each employee name
+    if (name === employee.name) {
+      //if the parameter name is equal to the object key name
+      individualBonus.push(`The employee name is ${employee.name}`);
+
+      let extraBonus = 0;
+      if (employee.employeeNumber.length <= 3) {
+        extraBonus = 5;
+      } else {
+        extraBonus = 0;
+      };
+      //The `bonusPercentage` property should contain the bonus percentage the employee is to receive.
+      //See section below for calculation instructions.
+      let bonusPercentage = 0;
+      if (employee.reviewRating <= 2) {
+        bonusPercentage = 0 + extraBonus;
+      } else if (employee.reviewRating === 3) {
+        bonusPercentage = 4 + extraBonus;
+      } else if (employee.reviewRating === 4) {
+        bonusPercentage = 6 + extraBonus;
+      } else if (employee.reviewRating === 5) {
+        bonusPercentage = 10 + extraBonus;
+      }
+      individualBonus.push(`The bonus percentage is ${bonusPercentage}`);
+
+      //pushing the individual bonus of the employee that qualifies under conditions below
+      let totalBonus = 0;
+      if (employee.reviewRating <= 2) {
+        totalBonus = 0 + extraBonus;
+      } else if (employee.reviewRating === 3) {
+        totalBonus = ((4 + extraBonus) / 100) * Number(employee.annualSalary);
+      } else if (employee.reviewRating === 4) {
+        totalBonus = ((6 + extraBonus) / 100) * Number(employee.annualSalary);
+      } else if (employee.reviewRating === 5) {
+        totalBonus = ((10 + extraBonus) / 100) * Number(employee.annualSalary);
+      }
+
+      individualBonus.push(`The total bonus is ${totalBonus}`);
+
+      let totalCompensation = totalBonus + Number(employee.annualSalary);
+      individualBonus.push(`Total compensation is ${totalCompensation}`);
     }
-    return individualBonus;
   }
+  return individualBonus;
 }
 
 console.log(bonusCalculator('Atticus'));
+console.log(bonusCalculator('Jem'));
+console.log(bonusCalculator('Scout'));
+console.log(bonusCalculator('Robert'));
+console.log(bonusCalculator("Mayella"));
 
+//property should be the adjusted annual compensation (base annual + bonus)
 // function individualBonusCalculation(number){
-  
+//The `totalBonus` should be the employee's total bonus rounded to the nearest dollar.  
 // } 
-  
-// console.log(individualBonusCalculation(4))
-// } 
-  
-// console.log(individualBonusCalculation(4))
+ //If their employee number is 4 digits long, this means they have been with the company for longer than 15 years,
+//and should receive an additional 5%.
+//- However, if their annual income is greater than $65,000, they should have their bonus adjusted down 1%.
+//- No bonus can be above 13% or below 0% total.
+ 
